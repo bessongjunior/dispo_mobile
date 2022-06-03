14532e15156d65a81aa65d1c64ee83d45f5f374b
BESSONG ATABE JUNIOR <juniorbesong8@hotmail.com>
2022-06-03 18:35:38 +0100

    Splashpage impl

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:dispo_mobile/pages/splash.dart';
 
 void main() {
-  runApp(const HomePage());
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
-        // title: 'Flutter App',
-        home: SplashPage()
+      home: SplashPage(goToPage: const WelcomePage(), duration: 4)));
+}
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        child: Center(
+          child: const Text('hello to you'),
+        ),
+      ),
     );
   }
 }
 
 
-class IndexPage extends StatefulWidget {
-  const IndexPage({Key? key}) : super(key: key);
 
-  @override
-  State<IndexPage> createState() => _IndexPageState();
-}
 
-class _IndexPageState extends State<IndexPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
+// class HomePage extends StatelessWidget {
+//   const HomePage({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//         // title: 'Flutter App',
+//         home: SplashPage(goToPage: const WelcomePage(), duration: 4)));
+//     );
+//   }
+// }
+//
+//
+// class WelcomePage extends StatefulWidget {
+//   const WelcomePage({Key? key}) : super(key: key);
+//
+//   @override
+//   State<WelcomePage> createState() => _WelcomePageState();
+// }
+//
+// class _WelcomePageState extends State<WelcomePage> with SingleTickerProviderStateMixin {
+//   late AnimationController _controller;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _controller = AnimationController(vsync: this);
+//   }
+//
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Text('hello');
+//   }
+// }
+//
 
-class SplashPage extends StatelessWidget {
-  const SplashPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: const Text('hello!'),
-    );
-  }
-}
