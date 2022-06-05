7be0b16226860e2b46bcb0921a91eede790f6143
BESSONG ATABE JUNIOR <juniorbesong8@hotmail.com>
2022-06-05 21:19:24 +0100

    profile page update

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
 
 
 class ProfilePage extends StatefulWidget {
@@ -11,6 +12,93 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 115,
+              width: 115,
+              child: Stack(
+                fit: StackFit.expand,
+                clipBehavior: Clip.none,
+                children: const <Widget> [
+                  CircleAvatar(
+                    backgroundImage: AssetImage("assets/images/Profile Image.png"),
+                  ),
+                  // Positioned(
+                  //   right: -16,
+                  //   bottom: 0,
+                  //   child: SizedBox(
+                  //     height: 46,
+                  //     width: 46,
+                  //     child: TextButton(
+                  //       style: TextButton.styleFrom(
+                  //         shape: RoundedRectangleBorder(
+                  //           borderRadius: BorderRadius.circular(50),
+                  //           side: const BorderSide(color: Colors.white),
+                  //         ),
+                  //         primary: Colors.white,
+                  //         backgroundColor: const Color(0xFFF5F6F9),
+                  //       ),
+                  //       onPressed: () {}, child: null,
+                  //     ),
+                  //   ),
+                  // )
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+class ProfileMenu extends StatelessWidget {
+  const ProfileMenu({
+    Key? key,
+    required this.text,
+    required this.icon,
+    this.press,
+  }) : super(key: key);
+
+  final String text, icon;
+  final VoidCallback? press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextButton(
+        style: TextButton.styleFrom(
+          primary: Colors.white,
+          padding: const EdgeInsets.all(20),
+          shape:
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          backgroundColor: const Color(0xFFF5F6F9),
+        ),
+        onPressed: press,
+        child: Row(
+          children: [
+            SvgPicture.asset(
+              icon,
+              color: Colors.white,
+              width: 22,
+            ),
+            const SizedBox(width: 20),
+            Expanded(child: Text(text)),
+            const Icon(Icons.arrow_forward_ios),
+          ],
+        ),
+      ),
+    );
   }
 }
