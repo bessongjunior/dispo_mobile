9ca79948f2f8a55f1af7af4dd3ae63dc93e8682f
BESSONG ATABE JUNIOR <juniorbesong8@hotmail.com>
2022-06-11 13:54:05 +0100

    signup page UI

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,4 +1,12 @@
+// ignore_for_file: non_constant_identifier_names, library_private_types_in_public_api
 import 'package:flutter/material.dart';
+import 'package:dispo_mobile/utils/formerror.dart';
+import 'package:dispo_mobile/widgets/themebutton.dart';
+import 'package:dispo_mobile/utils/constants.dart';
+import 'package:dispo_mobile/utils/sizeconfig.dart';
+import 'package:dispo_mobile/widgets/sociallogin.dart';
+
+
 
 class RegistrationPage extends StatefulWidget {
   const RegistrationPage({Key? key}) : super(key: key);
@@ -12,11 +20,220 @@ class _RegistrationPageState extends State<RegistrationPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(),
-        body: Container(
-          child: Center(
-            child: const Text('registration Page'),
+        body: SafeArea(
+          child: SizedBox(
+            width: double.infinity,
+            child: Padding(
+              padding:
+              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    SizedBox(height: SizeConfig.screenHeight * 0.04), // 4%
+                    Text("Register Account", style: headingStyle),
+                    const Text(
+                      "Complete your details or continue \nwith social media",
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: SizeConfig.screenHeight * 0.08),
+                    const SignUpForm(),
+                    SizedBox(height: SizeConfig.screenHeight * 0.08),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SocialCard(
+                          icon: "assets/icons/google-icon.svg",
+                          press: () {},
+                        ),
+                        SocialCard(
+                          icon: "assets/icons/facebook-2.svg",
+                          press: () {},
+                        ),
+                        SocialCard(
+                          icon: "assets/icons/twitter.svg",
+                          press: () {},
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: getProportionateScreenHeight(20)),
+                    Text(
+                      'By continuing your confirm that you agree \nwith our Term and Condition',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.caption,
+                    )
+                  ],
+                ),
+              ),
+            ),
           ),
         )
+    );
+  }
+}
+
+
+
+
+
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({Key? key}) : super(key: key);
+
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  final _formKey = GlobalKey<FormState>();
+  String? email;
+  String? password;
+  String? confirm_password;
+  bool remember = false;
+  final List<String?> errors = [];
+
+  void addError({String? error}) {
+    if (!errors.contains(error)) {
+      setState(() {
+        errors.add(error);
+      });
+    }
+  }
+
+  void removeError({String? error}) {
+    if (errors.contains(error)) {
+      setState(() {
+        errors.remove(error);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          buildEmailFormField(),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          buildPasswordFormField(),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          buildConformPassFormField(),
+          FormError(errors: errors),
+          SizedBox(height: getProportionateScreenHeight(40)),
+          // DefaultButton(
+          //   text: "Continue",
+          //   press: () {
+          //     if (_formKey.currentState!.validate()) {
+          //       _formKey.currentState!.save();
+          //       // if all are valid then go to success screen
+          //       // Navigator.pushNamed(context, CompleteProfileScreen.routeName);
+          //     }
+          //   },
+          // ),
+          ThemeButton(
+            label: 'Continue',
+            highlight: Colors.purple[900],
+            color: Colors.deepPurple, //AppColors.MAIN_COLOR,
+            onClick: () {
+            //   Navigator.push(context,
+            //       MaterialPageRoute(builder: (context) => const HomePage()));
+            //   // Utils.mainAppNav.currentState!.pushNamed('/mainpage');
+             }
+          )
+        ],
+      ),
+    );
+  }
+
+  TextFormField buildConformPassFormField() {
+    return TextFormField(
+      obscureText: true,
+      onSaved: (newValue) => confirm_password = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: passNullError);
+        } else if (value.isNotEmpty && password == confirm_password) {
+          removeError(error: matchPassError);
+        }
+        confirm_password = value;
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          addError(error: passNullError);
+          return "";
+        } else if ((password != value)) {
+          addError(error: matchPassError);
+          return "";
+        }
+        return null;
+      },
+      decoration: const InputDecoration(
+        labelText: "Confirm Password",
+        hintText: "Re-enter your password",
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: Icon(Icons.lock)
+      ),
+    );
+  }
+
+  TextFormField buildPasswordFormField() {
+    return TextFormField(
+      obscureText: true,
+      onSaved: (newValue) => password = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: passNullError);
+        } else if (value.length >= 8) {
+          removeError(error: shortPassError);
+        }
+        password = value;
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          addError(error: passNullError);
+          return "";
+        } else if (value.length < 8) {
+          addError(error: shortPassError);
+          return "";
+        }
+        return null;
+      },
+      decoration: const InputDecoration(
+        labelText: "Password",
+        hintText: "Enter your password",
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: Icon(Icons.lock)
+      ),
+    );
+  }
+
+  TextFormField buildEmailFormField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      onSaved: (newValue) => email = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: emailNullError);
+        } else if (emailValidatorRegExp.hasMatch(value)) {
+          removeError(error: invalidEmailError);
+        }
+        return;
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          addError(error: emailNullError);
+          return "";
+        } else if (!emailValidatorRegExp.hasMatch(value)) {
+          addError(error: invalidEmailError);
+          return "";
+        }
+        return null;
+      },
+      decoration: const InputDecoration(
+        labelText: "Email",
+        hintText: "Enter your email",
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: Icon(Icons.mail)
+      ),
     );
   }
 }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:dispo_mobile/utils/sizeconfig.dart';
+
+const kPrimaryColor = Color(0xFFFF7643);
+const kPrimaryLightColor = Color(0xFFFFECDF);
+const kPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFFFFA53E), Color(0xFFFF7643)],
+);
+const kSecondaryColor = Color(0xFF979797);
+const kTextColor = Color(0xFF757575);
+
+const kAnimationDuration = Duration(milliseconds: 200);
+
+final headingStyle = TextStyle(
+  fontSize: getProportionateScreenWidth(28),
+  fontWeight: FontWeight.bold,
+  color: Colors.black,
+  height: 1.5,
+);
+
+const defaultDuration = Duration(milliseconds: 250);
+
+// Form Error
+final RegExp emailValidatorRegExp =
+    RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+const String emailNullError = "Please Enter your email";
+const String invalidEmailError = "Please Enter Valid Email";
+const String passNullError = "Please Enter your password";
+const String shortPassError = "Password is too short";
+const String matchPassError = "Passwords don't match";
+const String nameNullError = "Please Enter your name";
+const String phoneNumberNullError = "Please Enter your phone number";
+const String addressNullError = "Please Enter your address";
+
+final otpInputDecoration = InputDecoration(
+  contentPadding:
+      EdgeInsets.symmetric(vertical: getProportionateScreenWidth(15)),
+  border: outlineInputBorder(),
+  focusedBorder: outlineInputBorder(),
+  enabledBorder: outlineInputBorder(),
+);
+
+OutlineInputBorder outlineInputBorder() {
+  return OutlineInputBorder(
+    borderRadius: BorderRadius.circular(getProportionateScreenWidth(15)),
+    borderSide: BorderSide(color: kTextColor),
+  );
+}
+
+
+
+
+
