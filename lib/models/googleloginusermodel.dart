064cd39ab9b81e47b2ff090cda5be6f8bca42cb4
BESSONG ATABE JUNIOR <juniorbesong8@hotmail.com>
2022-06-11 15:59:50 +0100

    google login service, but not yet integrated

---
+++ lib/models/googleloginusermodel.dart
@@ -0,0 +1,17 @@
+class LoginUserModel {
+  String? displayName;
+  String? email;
+  String? phoneNumber;
+  String? photoUrl;
+  String? uid;
+
+  LoginUserModel(
+      {
+        this.displayName,
+        this.email,
+        this.phoneNumber,
+        this.photoUrl,
+        this.uid
+      }
+      );
+}
