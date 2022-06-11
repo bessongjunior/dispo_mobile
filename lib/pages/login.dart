fa4b4566035d0612e39b9f27bc90fb3c464d748d
BESSONG ATABE JUNIOR <juniorbesong8@hotmail.com>
2022-06-11 14:08:09 +0100

    signin page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,11 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
+import 'package:dispo_mobile/utils/formerror.dart';
+import 'package:dispo_mobile/widgets/themebutton.dart';
+import 'package:dispo_mobile/utils/constants.dart';
+import 'package:dispo_mobile/utils/sizeconfig.dart';
+import 'package:dispo_mobile/widgets/sociallogin.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -12,11 +19,235 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        child: Center(
-          child: const Text('Login Page'),
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: Padding(
+            padding:
+            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(height: SizeConfig.screenHeight * 0.04),
+                  Text(
+                    "Welcome Back",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: getProportionateScreenWidth(28),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Text(
+                    "Sign in with your email and password  \nor continue with social media",
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: SizeConfig.screenHeight * 0.08),
+                  const SignForm(),
+                  SizedBox(height: SizeConfig.screenHeight * 0.08),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SocialCard(
+                        icon: "assets/icons/google-icon.svg",
+                        press: () {},
+                      ),
+                      SocialCard(
+                        icon: "assets/icons/facebook-2.svg",
+                        press: () {},
+                      ),
+                      SocialCard(
+                        icon: "assets/icons/twitter.svg",
+                        press: () {},
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: getProportionateScreenHeight(20)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don’t have an account? ",
+                        style: TextStyle(fontSize: getProportionateScreenWidth(16)),
+                      ),
+                      GestureDetector(
+                        onTap: (){},// => Navigator.pushNamed(context, SignUpScreen.routeName),
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(
+                              fontSize: getProportionateScreenWidth(16),
+                              color: kPrimaryColor),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       )
+    );
+  }
+}
+
+
+
+
+
+
+
+
+class SignForm extends StatefulWidget {
+  const SignForm({Key? key}) : super(key: key);
+
+  @override
+  _SignFormState createState() => _SignFormState();
+}
+
+class _SignFormState extends State<SignForm> {
+  final _formKey = GlobalKey<FormState>();
+  String? email;
+  String? password;
+  bool? remember = false;
+  final List<String?> errors = [];
+
+  void addError({String? error}) {
+    if (!errors.contains(error)) {
+      setState(() {
+        errors.add(error);
+      });
+    }
+  }
+
+  void removeError({String? error}) {
+    if (errors.contains(error)) {
+      setState(() {
+        errors.remove(error);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          buildEmailFormField(),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          buildPasswordFormField(),
+          SizedBox(height: getProportionateScreenHeight(30)),
+          Row(
+            children: [
+              Checkbox(
+                value: remember,
+                activeColor: kPrimaryColor,
+                onChanged: (value) {
+                  setState(() {
+                    remember = value;
+                  });
+                },
+              ),
+              const Text("Remember me"),
+              const Spacer(),
+              GestureDetector(
+                // onTap: () => Navigator.pushNamed(
+                //     context, ForgotPasswordScreen.routeName),
+                onTap: () {},
+                child: const Text(
+                  "Forgot Password",
+                  style: TextStyle(decoration: TextDecoration.underline),
+                ),
+              )
+            ],
+          ),
+          FormError(errors: errors),
+          SizedBox(height: getProportionateScreenHeight(20)),
+          // DefaultButton(
+          //   text: "Continue",
+          //   press: () {
+          //     if (_formKey.currentState!.validate()) {
+          //       _formKey.currentState!.save();
+          //       // if all are valid then go to success screen
+          //       KeyboardUtil.hideKeyboard(context);
+          //       Navigator.pushNamed(context, LoginSuccessScreen.routeName);
+          //     }
+          //   },
+          // ),
+          ThemeButton(
+              label: 'Continue',
+              highlight: Colors.purple[900],
+              color: Colors.deepPurple, //AppColors.MAIN_COLOR,
+              onClick: () {
+                //   Navigator.push(context,
+                //       MaterialPageRoute(builder: (context) => const HomePage()));
+                //   // Utils.mainAppNav.currentState!.pushNamed('/mainpage');
+              }
+          )
+        ],
+      ),
+    );
+  }
+
+  TextFormField buildPasswordFormField() {
+    return TextFormField(
+      obscureText: true,
+      onSaved: (newValue) => password = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: passNullError);
+        } else if (value.length >= 8) {
+          removeError(error: shortPassError);
+        }
+        return;
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          addError(error: passNullError);
+          return "";
+        } else if (value.length < 8) {
+          addError(error: shortPassError);
+          return "";
+        }
+        return null;
+      },
+      decoration: const InputDecoration(
+        labelText: "Password",
+        hintText: "Enter your password",
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: Icon(Icons.lock)
+      ),
+    );
+  }
+
+  TextFormField buildEmailFormField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      onSaved: (newValue) => email = newValue,
+      onChanged: (value) {
+        if (value.isNotEmpty) {
+          removeError(error: emailNullError);
+        } else if (emailValidatorRegExp.hasMatch(value)) {
+          removeError(error: invalidEmailError);
+        }
+        return;
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          addError(error: emailNullError);
+          return "";
+        } else if (!emailValidatorRegExp.hasMatch(value)) {
+          addError(error: invalidEmailError);
+          return "";
+        }
+        return null;
+      },
+      decoration: const InputDecoration(
+        labelText: "Email",
+        hintText: "Enter your email",
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        suffixIcon: Icon(Icons.mail)
+      ),
     );
   }
 }
